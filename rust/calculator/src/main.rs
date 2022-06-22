033671c07d1c0bc1d9ec5059ddcf0d50bdfc7a73
Psyhackological <konradkon@duck.com>
2022-06-22 13:18:32 +0200

    Added finished Rust code

--- rust/calculator/src/main.rs
+++ rust/calculator/src/main.rs
@@ -1,32 +1,32 @@
 use std::env::{args, Args};
 
 fn main() {
-  let mut args: Args = args();
+    let mut args: Args = args();
 
-  let first = args.nth(1).unwrap(); // we want 2nd argument
-  let operator = args.nth(0).unwrap().chars().next().unwrap(); // we use next, then unwrap it, then we take iterator from strings, and we call next on it which returns a char
-  let second = args.nth(0).unwrap(); // then again call next()
+    let first = args.nth(1).unwrap(); // we want 2nd argument
+    let operator = args.nth(0).unwrap().chars().next().unwrap(); // we use next, then unwrap it, then we take iterator from strings, and we call next on it which returns a char
+    let second = args.nth(0).unwrap(); // then again call next()
 
-  let first_number = first.parse::<f32>().unwrap(); // converting to f32
-  let second_number = second.parse::<f32>().unwrap(); // this also
-  let result = operate(operator, first_number, second_number);
-  let output_string = output(first_number, operator, second_number, result);
-  println!("{:?}", output_string);
+    let first_number = first.parse::<f32>().unwrap(); // converting to f32
+    let second_number = second.parse::<f32>().unwrap(); // this also
+    let result = operate(operator, first_number, second_number);
+    let output_string = output(first_number, operator, second_number, result);
+    println!("{:?}", output_string);
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
-  match operator {
-    '+' => first_number + second_number,
-    '-' => first_number - second_number,
-    '/' => first_number / second_number,
-    '*' | 'x' | 'X' => first_number * second_number,
-    _ => panic!("Invalid operator used."),
-  }
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '/' => first_number / second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
+        _ => panic!("Invalid operator used."),
+    }
 }
 
 fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
-  format!(
-    "{} {} {} = {}",
-    first_number, operator, second_number, result
-  )
+    format!(
+        "{} {} {} = {}",
+        first_number, operator, second_number, result
+    )
 }
