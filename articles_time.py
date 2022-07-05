9857cb7fd0e9f0eb4d05789c9e7d2d81e2bc4b8a
Psyhackological <konradkon@duck.com>
2022-07-05 16:50:12 +0200

    Spaghetti code, now refactor part

--- articles_time.py
+++ articles_time.py
@@ -2,13 +2,66 @@ from requests import get
 from bs4 import BeautifulSoup
 import readtime
 
-url = "https://www.freecodecamp.org/news/how-to-setup-virtual-environments-in-python/"
-request_response = get(url)
-soup = BeautifulSoup(request_response.content, "html.parser")
-all_h1 = soup.find_all("h1")
-all_h2 = soup.find_all("h2")
-all_p = soup.find_all('p')
-all_pre = soup.find_all("pre")
-print(all_h1.text, all_h2.text, all_p.text, all_pre.text)
-# estimated_readtime = readtime.of_text(section_post_full_content)
-# print(section_post_full_content)
+
+class Article:
+    def __init__(self, url):
+        self.article_url = url
+        self.soup = BeautifulSoup(get(self.article_url).content, "html.parser")
+        self.title = self.soup.title.text
+        self.tupple_tags = self.find_all_text()
+        self.string_tags = []
+        self.convert_them_into_string()
+        self.mega_string = self.merge_into_mega_string()
+        self.img_readtime_s = self.image_readtime()
+        self.text_readtime_s = self.text_readtime()
+        self.total_readtime_s = self.text_readtime_s + self.img_readtime_s
+
+    def __str__(self):
+        seconds = self.total_readtime_s % (24 * 3600)
+        hour = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+
+        return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+    def find_all_text(self):
+        all_h1 = self.soup.find_all("h1")
+        all_h2 = self.soup.find_all("h2")
+        all_p = self.soup.find_all('p')
+        all_pre = self.soup.find_all("pre")
+
+        return all_h1, all_h2, all_p, all_pre
+
+    def text_readtime(self):
+        estimated_text_readtime = readtime.of_text(self.mega_string)
+        return estimated_text_readtime.seconds
+
+    def image_readtime(self):
+        # img_weight * num_images
+        # With img_weight starting at 12 and decreasing one second with each image encountered, with a minium img_weight of 3 seconds.
+        # all_img_readtime = [readtime.of_html(image).seconds for image in all_img_filtered]
+        # total_seconds = sum(all_img_readtime)
+
+        all_img_filtered = self.soup.find_all("img")[2:-1]
+        num_images = len(all_img_filtered)
+        img_weight = 12 - num_images + 1  # Adding 1, because I think when I have 1 image it is multiplied by 12, not 11.
+        if img_weight < 3:
+            img_weight = 3
+        print(img_weight, num_images)
+        total_seconds = img_weight * num_images
+
+        return total_seconds
+
+    def convert_them_into_string(self):
+        for object_tag in self.tupple_tags:
+            tuple_of_tup = tuple([tag.text.strip() for tag in object_tag if len(tag) != 0])
+            string_tag = "".join(tuple_of_tup)
+            self.string_tags.append(string_tag)
+
+    def merge_into_mega_string(self):
+        return "".join(self.string_tags)
+
+
+article1 = Article("https://www.freecodecamp.org/news/vimrc-configuration-guide-customize-your-vim-editor/")
+print(article1.title, article1)
--- total_duration.py
+++ total_duration.py
@@ -30,54 +30,55 @@ def add_everything(durations):
     return total_hours, total_minutes, total_seconds
 
 
-def calculate_total_time(h, m, s):
+def calculate_total_time(h=0, m=0, s=0):
     m += s // 60
     s %= 60
     h += m // 60
     m %= 60
 
-    if m < 10:
-        m = str(m)
-        m = f"0{m}"
-    if s < 10:
-        s = str(s)
-        s = f"0{s}"
+    # if m < 10:
+    #     m = str(m)
+    #     m = f"0{m}"
+    # if s < 10:
+    #     s = str(s)
+    #     s = f"0{s}"
+    #
+    # return f"{h}:{m}:{s}"
+    return "%d:%02d:%02d" % (h, m, s)
 
-    return f"{h}:{m}:{s}"
+if __name__ == "__main__":
+    completed_python = ("3:10:30",)
+    completed_linux = ("1:14:29",)
+    completed_rust = ("1:25:37",)
+    completed_big_o_notation = ("0:00:00",)
+    completed_bootstrap = ("0:00:00",)
+    completed_durations = (
+        *completed_python, *completed_linux, *completed_rust, *completed_big_o_notation, *completed_bootstrap)
 
+    total_python = ("3:10:30", "2:42:55", "1:22:23", "3:46:22")
+    total_linux = ("1:14:29", "2:47:56", "5:00:17", "4:41:24")
+    total_rust = ("1:25:37",)
+    total_big_o_notation = ("1:56:16",)
+    total_bootstrap = ("2:46:22",)
+    total_durations = (*total_python, *total_linux, *total_rust, *total_big_o_notation, *total_bootstrap)
 
-completed_python = ("3:10:30",)
-completed_linux = ("1:14:29",)
-completed_rust = ("1:25:37",)
-completed_big_o_notation = ("0:00:00",)
-completed_bootstrap = ("0:00:00",)
-completed_durations = (
-    *completed_python, *completed_linux, *completed_rust, *completed_big_o_notation, *completed_bootstrap)
+    sum_completed_python = sum_time(completed_python)
+    sum_completed_linux = sum_time(completed_linux)
+    sum_completed_rust = sum_time(completed_rust)
+    sum_completed_big_o_notation = sum_time(completed_big_o_notation)
+    sum_completed_bootstrap = sum_time(completed_bootstrap)
+    sum_durations = sum_time(completed_durations)
 
-total_python = ("3:10:30", "2:42:55", "1:22:23", "3:46:22")
-total_linux = ("1:14:29", "2:47:56", "5:00:17", "4:41:24")
-total_rust = ("1:25:37",)
-total_big_o_notation = ("1:56:16",)
-total_bootstrap = ("2:46:22",)
-total_durations = (*total_python, *total_linux, *total_rust, *total_big_o_notation, *total_bootstrap)
+    sum_total_python = sum_time(total_python)
+    sum_total_linux = sum_time(total_linux)
+    sum_total_rust = sum_time(total_rust)
+    sum_total_big_o_notation = sum_time(total_big_o_notation)
+    sum_total_bootstrap = sum_time(total_bootstrap)
+    sum_total = sum_time(total_durations)
 
-sum_completed_python = sum_time(completed_python)
-sum_completed_linux = sum_time(completed_linux)
-sum_completed_rust = sum_time(completed_rust)
-sum_completed_big_o_notation = sum_time(completed_big_o_notation)
-sum_completed_bootstrap = sum_time(completed_bootstrap)
-sum_durations = sum_time(completed_durations)
-
-sum_total_python = sum_time(total_python)
-sum_total_linux = sum_time(total_linux)
-sum_total_rust = sum_time(total_rust)
-sum_total_big_o_notation = sum_time(total_big_o_notation)
-sum_total_bootstrap = sum_time(total_bootstrap)
-sum_total = sum_time(total_durations)
-
-print(f"Python: {sum_completed_python} / {sum_total_python}")
-print(f"Linux: {sum_completed_linux} / {sum_total_linux}")
-print(f"Rust: {sum_completed_rust} / {sum_total_rust}")
-print(f"Big O Notation: {sum_completed_big_o_notation} / {sum_total_big_o_notation}")
-print(f"Bootstrap: {sum_completed_bootstrap} / {sum_total_bootstrap}")
-print(f"Total: {sum_durations} / {sum_total}")
+    print(f"Python: {sum_completed_python} / {sum_total_python}")
+    print(f"Linux: {sum_completed_linux} / {sum_total_linux}")
+    print(f"Rust: {sum_completed_rust} / {sum_total_rust}")
+    print(f"Big O Notation: {sum_completed_big_o_notation} / {sum_total_big_o_notation}")
+    print(f"Bootstrap: {sum_completed_bootstrap} / {sum_total_bootstrap}")
+    print(f"Total: {sum_durations} / {sum_total}")
