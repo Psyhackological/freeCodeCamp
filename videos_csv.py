6794aefb581446e70962cd83bb352898f737a1b0
Psyhackological <konradkon@duck.com>
2022-08-25 22:24:42 +0200

    Organized into functions

--- videos_csv.py
+++ videos_csv.py
@@ -1,12 +1,16 @@
 import csv
 
-with open("videos.csv", mode="r") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        print(
-            row["emote"],
-            row["technology"],
-            row["yt-link"],
-            row["duration"],
-            row["done"],
-        )
+def read_csv(file_name="videos.csv"):
+    with open(file_name, mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            print(
+                row["emote"],
+                row["technology"],
+                row["yt-link"],
+                row["duration"],
+                row["done"],
+            )
+
+if __name__ == "__main__":
+    read_csv()
